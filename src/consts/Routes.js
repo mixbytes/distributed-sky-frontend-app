b40a002ec80a43262c9575f296dc51d645d4cef0
Oladiy <bd3396121@gmail.com>
2021-03-16 21:19:03 +0300

    [ADD]: routes

---
+++ src/consts/Routes.js
@@ -0,0 +1,12 @@
+const Routes = {
+    Host: 'localhost:5052/',
+
+    AccountAdd: '/account_add',
+    ExtractIPFSHash: '/extract_ipfs_hash',
+    ExtractPNGFromIPFS: '/extract_png_from_ipfs',
+    Main: '/',
+    RegisterPilot: '/register_pilot',
+    UploadToIPFS: '/upload_to_ipfs',
+};
+
+export default Routes;
