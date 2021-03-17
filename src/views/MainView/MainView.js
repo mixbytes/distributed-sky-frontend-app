d6228f1f29ab28f9f414702a18e65a33f354b2b4
Oladiy <bd3396121@gmail.com>
2021-03-17 12:17:10 +0300

    [ADD]: base view, main view (template, class)

---
+++ src/views/MainView/MainView.js
@@ -0,0 +1,28 @@
+import BaseView from 'views/BaseView/BaseView';
+import Menu from 'components/Menu/Menu';
+import template from 'views/MainView/MainView.hbs';
+
+export default class MainView extends BaseView {
+    /**
+     * Constructor of the main page view
+     * @constructor
+     * @param {string} title - title of the main page
+     */
+    constructor(title = 'DistributedSky') {
+        super(title);
+        this._template = template;
+    }
+
+    /**
+     * Method that shows main page view
+     */
+    async show() {
+        this._menu = new Menu();
+
+        const data = {
+            Menu: this._menu.render(),
+        };
+
+        await super.show(this._template(data));
+    }
+}
