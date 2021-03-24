a62c814543c770619e4b52011b11403d978833ec
Oladiy <bd3396121@gmail.com>
2021-03-24 19:40:22 +0300

    [UPDATE]: removed ipfs-hash lib, used ipfs.add result instead

--- src/utils/ManagerIPFS.js
+++ src/utils/ManagerIPFS.js
@@ -1,5 +1,4 @@
 import ipfsClient from 'ipfs-http-client';
-// import Hash from 'ipfs-only-hash';
 import toBuffer from 'it-to-buffer';
 
 const ipfs = ipfsClient({
@@ -12,15 +11,15 @@ export default class ManagerIPFS {
     async uploadToIPFS(fileMetadata) {
         const file = {path: fileMetadata.name, content: fileMetadata.data};
         let error = '';
-        await ipfs.add(file).catch((err) => {
+        const ipfsHash = await ipfs.add(file).catch((err) => {
             error = err;
         });
 
         if (error.length !== 0) {
             return error;
         }
-        return fileMetadata.data;
-        // return await Hash.of(fileMetadata.data);
+
+        return ipfsHash.cid.string;
     }
 
     async extractFromIPFS(hash) {
